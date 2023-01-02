fb0df37c20201ff142e43f92e614c6d64b1c32e0
pablovera866 <pablovera@outlook.com>
2023-01-02 19:17:50 -0300

    Subida del proyeto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,22 @@
+let express = require("express");
+let app = express();
+
+let path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3001, () => {
+  console.log("Servidor corriendo");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
