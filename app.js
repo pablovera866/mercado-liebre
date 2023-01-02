f8fa35c0fea9d89bbd3af478609ef4d7ec62cc0b
pablovera866 <pablovera@outlook.com>
2023-01-02 19:58:29 -0300

    Agrego gitignore

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3001, () => {
+app.listen(3000, () => {
   console.log("Servidor corriendo");
 });
 
