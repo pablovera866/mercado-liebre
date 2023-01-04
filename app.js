12b13f57061d3c429517ea137990c9cbec32e8e4
pablovera866 <pablovera@outlook.com>
2023-01-04 15:53:15 -0300

    Configurado el PORT

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ let path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 app.get("/", (req, res) => {
